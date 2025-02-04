19a25b2e2c337835dbffcab13f0c081f0582474b
tashakibanova <ov.nattie@gmail.com>
2025-02-04 23:07:50 +0500

    ver08

--- src/App.tsx
+++ src/App.tsx
@@ -1,4 +1,4 @@
-import { BrowserRouter } from "react-router-dom";
+import { HashRouter } from "react-router-dom";
 import { Provider } from "react-redux";
 import { store } from "./store/index";
 import { AppRoutes } from "./routes/AppRoutes";
@@ -6,11 +6,11 @@ import './App.css';
 
 function App() {
   return (
-    <BrowserRouter basename={import.meta.env.VITE_PUBLIC_URL}>
+    <HashRouter basename={import.meta.env.VITE_PUBLIC_URL}>
       <Provider store={store}>
         <AppRoutes />
       </Provider>
-    </BrowserRouter>
+    </HashRouter>
   );
 }
 
